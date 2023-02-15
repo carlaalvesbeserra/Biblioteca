946f30bc0c4325b31f1b58d6e53adf76c087df32
carlaalvesb <carlaalvesb1997@gmail.com>
2023-02-15 19:56:15 -0300

    Initial commit

---
+++ Models/UsuarioService.cs
@@ -0,0 +1,45 @@
+using System.Linq;
+using System.Collections.Generic;
+using System.Collections;
+
+namespace Biblioteca.Models
+{
+    public class UsuarioService
+    {
+        public void Inserir(Usuario u)
+        {
+            using(BibliotecaContext uu = new BibliotecaContext())
+            {
+                uu.Usuario.Add(u);
+                uu.SaveChanges();
+            }
+        }
+
+        public void Atualizar(Usuario u)
+        {
+            using(BibliotecaContext uu = new BibliotecaContext())
+            {
+                Usuario usuario = uu.Usuario.Find(u.Id);
+                usuario.loginUsuario = u.loginUsuario;
+                usuario.senhaUsuario = u.senhaUsuario;
+
+                uu.SaveChanges();
+            }
+        }
+        
+        public Usuario ObterPorId(int id)
+        {
+            using(BibliotecaContext uu = new BibliotecaContext())
+            {
+                return uu.Usuario.Find(id);
+            }
+        }
+
+        public ICollection <Usuario> Listar(){
+            using(BibliotecaContext bc = new BibliotecaContext()){
+                ICollection<Usuario> consulta = bc.Usuario.ToList();
+                return consulta;
+            }
+        }
+    }
+}
